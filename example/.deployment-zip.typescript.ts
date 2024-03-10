c802fd5130501713ac8ab18244e35351e24eb8bd
rutan <ru_shalm@hazimu.com>
2024-03-10 18:24:20 +0900

    add insertTagToHTMLHeadPlugin

--- example/.deployment-zip.typescript.ts
+++ example/.deployment-zip.typescript.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from '../src/config.js';
+import { insertTagToHTMLHeadPlugin } from '../src/plugins/index.js';
 import { readStreamText } from '../src/utils.js';
 
 export default defineConfig({
@@ -10,6 +11,25 @@ export default defineConfig({
   },
   ignores: ['.*', '*.dat', 'skip.txt', 'abc/file4.txt'],
   plugins: [
+    insertTagToHTMLHeadPlugin({
+      prepend: [
+        {
+          tag: 'script',
+          attributes: {
+            src: 'https://example.com/script.js',
+          },
+        },
+      ],
+      append: [
+        {
+          tag: 'link',
+          attributes: {
+            rel: 'stylesheet',
+            href: 'https://example.com/style.css',
+          },
+        },
+      ],
+    }),
     {
       transform: async ({ name, stream }) => {
         if (!name.endsWith('.md')) return;
--- src/plugin.ts
+++ src/plugin.ts
@@ -3,6 +3,7 @@ import type { Config } from './config.js';
 import type { DeploymentMode, ParametersType, Promisable } from './types.js';
 
 export type Plugin = {
+  name?: string;
   options?: (config: Config) => Promisable<Config | undefined>;
   deployStart?: (config: Config) => void;
   deployEnd?: (error?: Error) => void;
@@ -12,8 +13,9 @@ export type Plugin = {
     mode: DeploymentMode;
   }) => Promisable<Readable | string | undefined>;
 };
+type PluginHooks = Omit<Plugin, 'name'>;
 
-export async function callPluginHook<T extends keyof Plugin>({
+export async function callPluginHook<T extends keyof PluginHooks>({
   plugins,
   hook: hookName,
   args,
@@ -21,11 +23,11 @@ export async function callPluginHook<T extends keyof Plugin>({
 }: {
   plugins?: Plugin[];
   hook: T;
-  args: ParametersType<Required<Plugin>>[T];
+  args: ParametersType<Required<PluginHooks>>[T];
   argsHook?: (
-    currentArgs: ParametersType<Required<Plugin>>[T],
-    response: Awaited<ReturnType<Required<Plugin>[T]>>,
-  ) => ParametersType<Required<Plugin>>[T];
+    currentArgs: ParametersType<Required<PluginHooks>>[T],
+    response: Awaited<ReturnType<Required<PluginHooks>[T]>>,
+  ) => ParametersType<Required<PluginHooks>>[T];
 }) {
   if (!plugins) return args;
 
@@ -34,7 +36,7 @@ export async function callPluginHook<T extends keyof Plugin>({
   for (const plugin of plugins) {
     const hook = plugin[hookName];
     if (hook) {
-      const result = await (hook as (...args: any[]) => ReturnType<Required<Plugin>[T]>)(...customArgs);
+      const result = await (hook as (...args: any[]) => ReturnType<Required<PluginHooks>[T]>)(...customArgs);
       if (result === undefined || !argsHook) continue;
       customArgs = argsHook(customArgs, result);
     }
